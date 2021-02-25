28970fc49b65e19aaffe6dc6679cf7fc4d0f28b6
elena <bazhanovd@yandex.ru>
2021-02-26 02:47:27 +0400

    0.161

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -4,8 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('redoc/',
-         TemplateView.as_view(template_name='redoc.html'),
-         name='redoc'),
+    path('redoc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redoc'),
     path('api/', include('api.urls', )),
 ]
